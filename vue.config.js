38df51f3410c388b0cfba93e69ed2a0d3f834a29
sophia <sophia@ready-market.com>
2022-06-30 18:09:45 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -7,4 +7,15 @@ module.exports = {
     }
   },
   publicPath: '/profile/',
+  configureWebpack: config => {
+      // 为生产环境修改配置...
+      if (process.env.NODE_ENV === 'production') {
+        config.mode = 'production';
+        // 打包文件大小配置
+        config.performance = {
+          maxEntrypointSize: 10000000,
+          maxAssetSize: 30000000
+        }
+      }
+  },
 };
